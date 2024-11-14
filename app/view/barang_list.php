954d7df22530df75e48f32edd9809bf273f0319f
AndreasTopuh <andreasjeno23@gmail.com>
2024-11-14 14:54:33 +0800

    menambahkan fungsi ke nomor telefon dengan teks otomatis

--- app/view/barang_list.php
+++ app/view/barang_list.php
@@ -47,7 +47,8 @@ $barangList = $barangController->showAllBarang();
                                     Deskripsi: <?= htmlspecialchars($barang['deskripsi']); ?>
                                 </p>
                                 <a href="#" class="card-link">Lihat Detail</a>
-                            <a href="https://wa.me/<?= urlencode($barang['nomor_penjual']); ?>" class="card-link" target="_blank">Hubungi Penjual</a>
+                                <a href="https://wa.me/<?= urlencode($barang['nomor_penjual']); ?>?text=Halo,%20saya%20tertarik%20dengan%20barang%20yang%20Anda%20jual." class="card-link" target="_blank">Hubungi Penjual</a>
+
                             </div>
                         </div>
                     </div>
