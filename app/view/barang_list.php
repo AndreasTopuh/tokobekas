bea7b355fc9111a6a81aea394cf8ac1653fb3294
AndreasTopuh <andreasjeno23@gmail.com>
2024-11-18 07:23:38 +0800

    adding logo di web title

--- app/view/barang_list.php
+++ app/view/barang_list.php
@@ -24,6 +24,8 @@ $user = $_SESSION['user'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+     <link rel="icon" href="/tokobekas/public/images/logo-tokobekas.png" type="image/png">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/tokobekas/public/css/style.css">
     <link rel="stylesheet" href="/tokobekas/public/css/tiny-slider.css">
--- app/view/register.php
+++ app/view/register.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+     <link rel="icon" href="/tokobekas/public/images/logo-tokobekas.png" type="image/png">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <style>
         body {
